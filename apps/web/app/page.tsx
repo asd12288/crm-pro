ef1050018e42a32dbfc68c530f1e00540d87515d
asd12288 <ilanchelly@gmail.com>
2025-12-27 22:28:05 +0200

    feat: add new turbo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@workspace/ui/components/button"
+import { Button } from "@workspace/ui/components/button";
+import { add } from "@workspace/math/add";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">Hello Apps web</h1>
+        <Button size="sm">{add(1, 2)}</Button>
       </div>
     </div>
-  )
+  );
 }
